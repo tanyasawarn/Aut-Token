de4a720ae4b6164e203654c68daa8a19fc8f8449
tanyasawarn <121800861+tanyasawarn@users.noreply.github.com>
2023-05-03 21:42:03 +0530

    Add files via upload

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,24 +1,39 @@
-import { Link } from 'react-router-dom';
-
-import classes from './MainNavigation.module.css';
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import AuthContext from "../store/authcontext";
+import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
+  const navigate = useNavigate();
+  const authCtx = useContext(AuthContext);
+  const logoutHandler = () =>{
+    authCtx.logout();
+    navigate('/login');
+  }
+  const isLoggedIn = authCtx.isLoggedIn;
   return (
     <header className={classes.header}>
-      <Link to='/'>
+      <Link to="/">
         <div className={classes.logo}>React Auth</div>
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link to='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link to='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!isLoggedIn && (
+            <li>
+              <Link to="/auth">Login</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
